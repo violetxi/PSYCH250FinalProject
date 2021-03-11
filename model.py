bfdfd3071f961d463cfd8d6cd60ce393dcf2e126
Violet (Ziyu) Xiang <ziyxiang@node12-ccncluster.stanford.edu>
2021-03-11 14:10:20 -0800

    minor

--- model.py
+++ model.py
@@ -9,8 +9,8 @@ import torchvision.models as models
 class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
-        self.model = models.resnet18(pretrained=True)
-        self.model.fc = nn.Linear(512, 4)    # 4 classes in fLoc dataset
+        self.model = models.alexnet(pretrained=True)
+        self.model.fc = nn.Linear(4096, 4)    # 4 classes in fLoc dataset
 
     def forward(self, x):
         x = self.model(x)
