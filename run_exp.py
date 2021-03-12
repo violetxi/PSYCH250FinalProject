e927adb1389fa7de9c27b1bf0be8ede17e926608
Violet (Ziyu) Xiang <ziyxiang@node12-ccncluster.stanford.edu>
2021-03-12 01:40:58 -0800

    add label based evaluation & results

--- run_exp.py
+++ run_exp.py
@@ -1,6 +1,8 @@
 import os
+import pickle
 import argparse
 import numpy as np
+import pandas as pd
 from tqdm import tqdm
 
 import torch
@@ -14,6 +16,9 @@ from torchvision.utils import save_image
 from model import Model
 from dataset import StimuliDataset
 
+LABEL_MAP = {0 : 'faces', 1 : 'bodies',
+             2 : 'places', 3 : 'characters'}
+
 def load_args():
     parser = argparse.ArgumentParser(description='Leison study on AlexNet')
     parser.add_argument('--ckpt_folder', type=str,
@@ -22,18 +27,21 @@ def load_args():
                         help='Where to save trained models.')
     parser.add_argument('--meta_path', type=str,
                         help="Where processed PT files are.")
+    parser.add_argument('--result_by_label', action='store_true',
+                        help='Whether to store results by label')
     args = parser.parse_args()
     return args
 
 
 class RunExp:
-    def __init__(self, args, n_random_lesions, lesion_percent=0.2, seed=0):
+    def __init__(self, args, n_exps, lesion_percent=0.2, seed=0):
         np.random.seed(seed)    # to reproduce the randomly sampled results
         self.args = args
         self.ckpt_paths = self.get_all_checkpoint_paths()
         self.lesion_percent = lesion_percent
-        self.n_random_lesions = n_random_lesions
-        # 5 conv layers to be lesioned
+        self.n_exps = n_exps
+        self.result_by_label = self.args.result_by_label
+        # 5 conv layers to be lesioned        
         self.target_layers = [
             'model.features.0', 'model.features.3', 'model.features.6',
             'model.features.8', 'model.features.10']
@@ -57,9 +65,12 @@ class RunExp:
     def load_data(self):
         dataset = StimuliDataset(self.args.meta_path, train=False)
         self.dataloader = DataLoader(
-            dataset, batch_size=200, shuffle=False, num_workers=0)
+            dataset, batch_size=1, shuffle=False, num_workers=0)
 
-    def eval_model(self):        
+    def eval_model(self, untrained=False):
+        if untrained:
+            self.model = Model(pretrained=False)
+            self.model.cuda()
         correct = 0
         total = 0
         for i, (ims, labels) in enumerate(self.dataloader):
@@ -88,25 +99,107 @@ class RunExp:
 
     # lesion experiments are only done on the imgnt-pretrained or trained net
     def run_lesion_exps(self):
-        for ckpt_path in self.ckpt_paths:
-            self.load_model(ckpt_path)            
-            for layer in self.target_layers[:2]:
-                self.model_state_dict = self.model.state_dict()    # keep tracking of modified state dict
-                self.lesion_one_layer(layer)
-                self.model.load_state_dict(self.model_state_dict)
-                accuracy = self.eval_model()
-                print(ckpt_path, layer, accuracy)
-    
-    def eval_ckpts(self):        
-        for ckpt_path in self.ckpt_paths:
+        print('Running lesion experiments')
+        for ckpt_path in tqdm(self.ckpt_paths):            
             self.load_model(ckpt_path)
-            accuracy = self.eval_model()
-            print(f'{ckpt_path} validataion accuracy: {accuracy}')
-    
+            epoch = os.path.basename(ckpt_path).split('.')[0]
+            if self.result_by_label:
+                result_path = os.path.join(
+                    self.args.result_folder,
+                    f'lesion_exp_category_epoch_{epoch}.pkl'
+                )
+            else:
+                result_path = os.path.join(
+                    self.args.result_folder,
+                    f'lesion_exp_overall_epoch_{epoch}.csv'
+                )
+            results = {}
+            index = []
+            for layer in tqdm(self.target_layers):             
+                index.append(layer)
+                for n_exp in range(self.n_exps):
+                    exp = f'exp_{n_exp}'
+                    self.model_state_dict = self.model.state_dict()    # keep tracking of modified state dict
+                    self.lesion_one_layer(layer)
+                    self.model.load_state_dict(self.model_state_dict)
+                    if self.result_by_label:
+                        accuracy = self.eval_model_by_label()
+                    else:
+                        accuracy = self.eval_model()
+                    if exp not in results:
+                        results[exp] = [accuracy]
+                    else:
+                        results[exp].append(accuracy)
+                
+            self.store_results(results, result_path, index)
+                    
+    def eval_ckpts(self):
+        print('evaluating checkpoints')
+        if self.result_by_label:
+            accuracy = self.eval_model_by_label(untrained=True)
+            result_path = f'{self.args.result_folder}/epoch_results_by_label.csv'
+            results = {}
+            results['untarined'] = accuracy
+        else:
+            accuracy = self.eval_model(untrained=True)
+            result_path = f'{self.args.result_folder}/epoch_results_overall.csv'
+            results = {'epoch': [], 'accuracy': []}        
+            results['epoch'].append('untrained')
+            results['accuracy'].append(accuracy)
+        
+        for ckpt_path in tqdm(self.ckpt_paths):
+            self.load_model(ckpt_path)
+            epoch = int(os.path.basename(ckpt_path).split('.')[0])
+            if self.result_by_label:
+                accuracy = self.eval_model_by_label()
+                results[epoch] = accuracy
+            else:
+                accuracy = self.eval_model()            
+                results['epoch'].append(epoch)
+                results['accuracy'].append(accuracy)
 
+        self.store_results(results, result_path)
+
+    def eval_model_by_label(self, untrained=False):
+        if untrained:
+            self.model = Model(pretrained=False)
+            self.model.cuda()
+        label_result = {
+            label : {'correct': 0, 'total': 0} for label in LABEL_MAP
+        }
+        correct = 0
+        total = 0
+        for i, (ims, labels) in enumerate(self.dataloader):
+            ims = ims.cuda()            
+            labels = labels.cuda()
+            outputs = self.model(ims)
+            preds = torch.argmax(outputs, 1)
+            label_result[labels.item()]['total'] += preds.size()[0]
+            correct = (preds == labels).sum().item()
+            label_result[labels.item()]['correct'] += correct
+
+        final_label_result = {LABEL_MAP[label] : label_result[label]['correct'] /
+                              label_result[label]['total'] for label in label_result}
+        return final_label_result
+        
+    def store_results(self, results, path, index=None):
+        print(path)
+        if self.result_by_label and path.endswith('pkl'):
+            pickle.dump(results, open(path, 'wb'))
+        else:
+            if index:
+                df = pd.DataFrame(results, index=index)
+            else:
+                df = pd.DataFrame(results)
+            df.to_csv(path)
+
+    def do_exps(self):
+        self.eval_ckpts()
+        self.run_lesion_exps()
+    
+    
 if __name__ == '__main__':
     args = load_args()
     n_random_lesions = 10
     run_exp = RunExp(args, n_random_lesions)
-    #run_exp.eval_ckpts()
-    run_exp.run_lesion_exps()
+    run_exp.do_exps()
